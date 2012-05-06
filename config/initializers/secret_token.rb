e902f68e8bb51660db2e0e1d6bcbb64b152eb3a1
Patrick Aoun <patrick.aoun@etu.enseeiht.fr>
2012-05-06 12:33:56 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectIiBlog::Application.config.secret_token = 'ad10e15538b266b030fbdeeae72200962c29c3225cdedea4316a0ec5599d3f4eaeafee7c3547b10f37fec225464d1cfb3f95a0fa10e3de952d95716bea7ea1d2'
