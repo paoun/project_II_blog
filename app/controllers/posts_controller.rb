c401f6b6e28c28af0200c169daf1f1290ed7d8aa
Patrick Aoun <patrick.aoun@etu.enseeiht.fr>
2012-05-14 09:50:06 +0200

    Scenario 2 : création de post / OK

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
  	end
+
+	def new
+		@posts = Post.new
+	end
+
+	def create
+		post = Post.create(params[:post])
+		redirect_to(posts_path)
+	end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,21 +1,38 @@
 require 'spec_helper'
 describe PostsController do
-  describe "GET 'index'" do
-    before(:each) do
-      @posts = [stub_model(Post,:title => "1"), stub_model(Post, :title => "2")]
-      Post.stub(:all){ @posts }
-    end
-    it "assigns a list of posts" do
-      Post.should_receive(:all).and_return(@posts)
-      get 'index'
-      assigns(:posts).should eq @posts
-      response.should be_success
-    end
+  	describe "GET 'index'" do
+    	before(:each) do
+      		@posts = [stub_model(Post,:title => "1"), stub_model(Post, :title => "2")]
+      		Post.stub(:all){ @posts }
+		end
 
-    it "renders the template list" do
-      get 'index'
-      response.should render_template(:index)
-    end
-  end
+    	it "assigns a list of posts" do
+      		Post.should_receive(:all).and_return(@posts)
+      		get 'index'
+      		assigns(:posts).should eq @posts
+      		response.should be_success
+    	end
+
+    	it "renders the template list" do
+      		get 'index'
+      		response.should render_template(:index)
+    	end
+  	end
+
+	describe "GET 'new'" do
+
+		it "should use new" do
+			Post.should_receive(:new)
+			get 'new'
+			response.should be_success
+		end
+
+    	it "renders the template new" do
+      		get 'new'
+      		response.should render_template(:new)
+    	end
+
+	end
+
 end
 
